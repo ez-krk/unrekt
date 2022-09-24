48125017ae22c185a5b04b1c8729320dc0ef8aa6
EsyWin <dev@krk.finance>
2022-09-24 12:41:15 +0200

    changed pepe.png

--- src/components/pepe/pepe.component.jsx
+++ src/components/pepe/pepe.component.jsx
@@ -2,7 +2,7 @@ const Pepe = () => {
   return (
     <div className="max-w-[420px]">
       <img
-        src="https//raw.githubusercontent.com/esywin/unrekt/main/public/pepe.png"
+        src="https://raw.githubusercontent.com/EsyWin/unrekt/main/public/pepe.png"
         alt="pepe"
       />
     </div>
